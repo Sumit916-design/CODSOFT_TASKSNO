0d88daa5fb2a06bce2444c051011f9f777563253
Sumit916-design <sumitkumar993468@gmail.com>
2026-09-16 23:18:23 +0530

    Implement Student Grade Calculator
    This program calculates the total marks, average percentage, and grade based on user input for multiple subjects. It includes input validation for marks.

---
+++ Task2_StudentGradeCalculator.java
@@ -0,0 +1,64 @@
+import java.util.Scanner;
+
+public class StudentGradeCalculator {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter number of subjects: ");
+        int subjects = sc.nextInt();
+
+        int total = 0;
+
+        System.out.println("\nEnter marks out of 100:");
+
+        for (int i = 1; i <= subjects; i++) {
+
+            System.out.print("Enter marks for Subject " + i + ": ");
+            int marks = sc.nextInt();
+
+            // Validate marks
+            while (marks < 0 || marks > 100) {
+                System.out.println("Invalid marks! Enter marks between 0 and 100.");
+                System.out.print("Enter marks again: ");
+                marks = sc.nextInt();
+            }
+
+            total = total + marks;
+        }
+
+        // Calculate average percentage
+        double average = (double) total / subjects;
+
+        // Calculate grade
+        char grade;
+
+        if (average >= 90) {
+            grade = 'A';
+
+        } else if (average >= 80) {
+            grade = 'B';
+
+        } else if (average >= 70) {
+            grade = 'C';
+
+        } else if (average >= 60) {
+            grade = 'D';
+
+        } else if (average >= 50) {
+            grade = 'E';
+
+        } else {
+            grade = 'F';
+        }
+
+        // Display result
+        System.out.println("\n===== STUDENT RESULT =====");
+        System.out.println("Total Marks: " + total);
+        System.out.println("Average Percentage: " + average + "%");
+        System.out.println("Grade: " + grade);
+
+        sc.close();
+    }
+}
